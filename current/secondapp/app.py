68e4c04a389abe50e77b1b2b733c4a9ce17ab486
thedidev <thedidyev@gmail.com>
2024-09-11 21:17:55 +0300

    update

--- current/secondapp/app.py
+++ current/secondapp/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, send_from_directory
 import pandas as pd
+import os
+import uuid
 
 app = Flask(__name__, template_folder='templates')
 
@@ -47,11 +49,28 @@ def convert_csv():
                 'Content-Desposition': 'attachment; filename=result.csv'
             }
         )
-            
+
         return response
     else: 
         return "Unsiported file type", 400
+    
+@app.route('/convert_csv_two', methods=['POST'])
+def convert_csv_two():
+    file = request.files['file']
+    
+    df = pd.read_excel(file)
+    
+    if not os.path.exists('downloads'):
+        os.makedirs('downloads')
         
+    filename = f'{uuid.uuid4()}.csv'
+    df.to_csv(os.path.join('downloads', filename))
+    
+    return render_template('download.html', filename=filename)
+        
+@app.route('/download/<filename>')
+def download(filename):
+    return send_from_directory('download', filename, download_name='result.csv')
 
 if __name__ == "__main__":
     app.run('0.0.0.0', debug=True)
